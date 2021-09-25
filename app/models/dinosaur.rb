0fac1e2c4375f7ea7bd30c5523fa941e6551e593
= <pierreambroise@hotmail.fr>
2021-09-26 00:38:02 +0900

    testing

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -2,6 +2,6 @@ class Dinosaur < ApplicationRecord
   has_one_attached :photo
   acts_as_taggable_on :tags
 
-  dino_list = ["Dromaeosaurus", "Carnotaurus", "Oviraptor", "Sinraptor", "Velociraptor", "Megalosaurus", "Albertosaurus", "Ichthyornis", "Allosaurus", "Giganotosaurus", "Brachiosaurus", "Tyrannosaurus", "Camarasaurus", "Triceratops", "Psittacosaurus", "Styracosaurus", "Magnapaulia", "Nodosaurus", "Camptosaurus", "Pachycephalosaurus", "Iguanodon", "Stegosaurus", "Diplodocus", "Ankylosaurus", "Dilophosaurus", "Gorgosaurus", "Parasaur", "Spinosaurus" ]
+  dino_list = ["Dromaeosaurus", "Carnotaurus", "Oviraptor", "Sinraptor", "Velociraptor", "Megalosaurus", "Albertosaurus", "Ichthyornis", "Allosaurus", "Giganotosaurus", "Brachiosaurus", "Tyrannosaurus", "Camarasaurus", "Triceratops", "Psittacosaurus", "Styracosaurus", "Magnapaulia", "Nodosaurus", "Camptosaurus", "Pachycephalosaurus", "Iguanodon", "Stegosaurus", "Diplodocus", "Ankylosaurus", "Dilophosaurus", "Parasaur", "Spinosaurus" ]
   TAGS = dino_list.sort
 end
