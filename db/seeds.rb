6f411f820748b1a03e29e5c95a590803c4d135a9
= <pierreambroise@hotmail.fr>
2021-09-26 00:09:02 +0900

    trying to get the prod photo to load

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,49 +16,49 @@ filepath1 = 'db/diplodocuses.csv'
 filepath2 = 'db/stegosauruses.csv'
 filepath3 = 'db/Tyrannosauruses.csv'
 
-# CSV.foreach(filepath0, csv_options) do |row|
-#   Dinosaur.create!(
-#     name: row[9],
-#     # tag_list: 'Tyrannosaure' if dino.name.include?('Tyrann'),
-#     # tag_list: 'Diplodocus' if dino.name.include?('Diplodocus'),
-#     # tag_list: 'Ankylosaurus' if dino.name.include? ('Ankylosaurus'),
-#     # tag_list: 'Dilophosaurus' if dino.name.include? ('Dilophosaurus'),
-#     # tag_list: 'Gorgosaurus' if dino.name.include? ('Gorgosaurus'),
-#     longitude: row[17],
-#     latitude: row[18]
-#   )
+CSV.foreach(filepath0, csv_options) do |row|
+  Dinosaur.create!(
+    name: row[9],
+    # tag_list: 'Tyrannosaure' if dino.name.include?('Tyrann'),
+    # tag_list: 'Diplodocus' if dino.name.include?('Diplodocus'),
+    # tag_list: 'Ankylosaurus' if dino.name.include? ('Ankylosaurus'),
+    # tag_list: 'Dilophosaurus' if dino.name.include? ('Dilophosaurus'),
+    # tag_list: 'Gorgosaurus' if dino.name.include? ('Gorgosaurus'),
+    longitude: row[17],
+    latitude: row[18]
+  )
 
-#   puts "#{Dinosaur.count} dinosaurs were created."
+  puts "#{Dinosaur.count} dinosaurs were created."
+end
+
+
+
+# CSV.foreach(filepath1, csv_options) do |row|
+#   Dinosaur.create!(
+#       name: row[9],
+#       longitude: row[17],
+#       latitude: row[18]
+#     )
+#   puts "#{Dinosaur.count} diplodocuses were created."
 # end
 
+# CSV.foreach(filepath2, csv_options) do |row|
+#   Dinosaur.create!(
+#       name: row[9],
+#       longitude: row[17],
+#       latitude: row[18]
+#     )
+#   puts "#{Dinosaur.count} stegosauruses were created."
+# end
 
-
-CSV.foreach(filepath1, csv_options) do |row|
-  Dinosaur.create!(
-      name: row[9],
-      longitude: row[17],
-      latitude: row[18]
-    )
-  puts "#{Dinosaur.count} diplodocuses were created."
-end
-
-CSV.foreach(filepath2, csv_options) do |row|
-  Dinosaur.create!(
-      name: row[9],
-      longitude: row[17],
-      latitude: row[18]
-    )
-  puts "#{Dinosaur.count} stegosauruses were created."
-end
-
-CSV.foreach(filepath3, csv_options) do |row|
-  Dinosaur.create!(
-      name: row[9],
-      longitude: row[17],
-      latitude: row[18]
-    )
-  puts "#{Dinosaur.count} Tyrannosauruses were created."
-end
+# CSV.foreach(filepath3, csv_options) do |row|
+#   Dinosaur.create!(
+#       name: row[9],
+#       longitude: row[17],
+#       latitude: row[18]
+#     )
+#   puts "#{Dinosaur.count} Tyrannosauruses were created."
+# end
 
 puts "#{Dinosaur.count} dinosaurs were created."
 
@@ -69,21 +69,24 @@ Dinosaur.all.each do |dino|
   dino.save
 end
 
-# Dinosaur::TAGS.each do |tag|
-#   dinos = Dinosaur.tagged_with(tag)
-#   if dinos.present?
-#   dinos.first.photo.attach(io: File.open(Rails.root.join("app/assets/images/#{tag}.jpeg")),
-#                   filename: "#{tag}.jpeg")
-#   end
-# end
+puts "#{tag} tagged"
 
 Dinosaur::TAGS.each do |tag|
   dinos = Dinosaur.tagged_with(tag)
   if dinos.present?
-    dinos.first.photo.attach(io: File.open(File.join(Rails.root,"app/assets/images/#{tag}.jpeg")),
-                        filename: "#{tag}.jpeg")
+  dinos.first.photo.attach(io: File.open(Rails.root.join("app/assets/images/#{tag}.jpeg")),
+                  filename: "#{tag}.jpeg")
+  puts "photo to #{tag} added."
   end
 end
+
+# Dinosaur::TAGS.each do |tag|
+#   dinos = Dinosaur.tagged_with(tag)
+#   if dinos.present?
+#     dinos.first.photo.attach(io: File.open(File.join(Rails.root,"app/assets/images/#{tag}.jpeg")),
+#                         filename: "#{tag}.jpeg")
+#   end
+# end
 
 # Dinosaur::TAGS.each do |tag|
 #   url = URI.open("https://res.cloudinary.com/hco3zesos/image/upload/v1632472948/#{tag}.jpg")
